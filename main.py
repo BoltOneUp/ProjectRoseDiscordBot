7c3a4370b4d298347b844794e29f6bf79c90c61b
Evan <evanwarrenlie@outlook.com>
2026-02-13 17:17:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ keep_alive()
 
 intents = discord.Intents.default()
 intents.members = True # Example for member access
-bot = bot.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
